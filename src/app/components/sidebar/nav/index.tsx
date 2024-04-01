48f1e1849ff28e7eb66cbb4d55d97809be89757c
Jess <Jessica.Q.Chiu@gmail.com>
2024-04-01 03:23:08 -0700

    Add nav icon links
    Adding in placeholder icon links for potential social media shortcuts.

--- src/app/components/sidebar/nav/index.tsx
+++ src/app/components/sidebar/nav/index.tsx
@@ -9,11 +9,20 @@ const Nav = ({ toggle }: { toggle: () => void }) => {
       <div className="max-md:hidden flex flex-col h-screen w-60">
         <div className="flex justify-center mb-12">
           <Link href="/">
-            <Image src="/images/logo.png" alt="Logo" width={200} height={200} priority={true} />
+            <Image
+              src="/images/logo.png"
+              alt="Logo"
+              width={200}
+              height={200}
+              priority={true}
+            />
           </Link>
         </div>
         <div className="flex flex-col mb-6 px-4 py-2 bg-white border-2 border-black rounded-lg shadow-[5px_5px_0px_0px_#fed7aa]">
-          <p>Hey there, I am Jess and welcome to my portfolio website.</p>
+          <p>
+            Hey there! I&apos;m Jess, a Vancouver-based developer and
+            illustrator.
+          </p>
         </div>
         <div className="flex flex-col px-4 py-2 bg-blue-200 border-2 border-black rounded-lg shadow-[5px_5px_0px_0px_#fed7aa]">
           <ul className="text-black">
@@ -33,6 +42,44 @@ const Nav = ({ toggle }: { toggle: () => void }) => {
               </Link>
             </li>
           </ul>
+          <div className="flex flex-row justify-between pt-2">
+            <div>
+            <Link href="/">
+                <Image
+                  src="/images/star.png"
+                  alt="Logo"
+                  width={30}
+                  height={30}
+                />
+              </Link>
+            </div>
+            <div className="flex flex-row">
+              <Link href="/">
+                <Image
+                  src="/images/star.png"
+                  alt="Logo"
+                  width={30}
+                  height={30}
+                />
+              </Link>
+              <Link href="/">
+                <Image
+                  src="/images/star.png"
+                  alt="Logo"
+                  width={30}
+                  height={30}
+                />
+              </Link>
+              <Link href="/">
+                <Image
+                  src="/images/star.png"
+                  alt="Logo"
+                  width={30}
+                  height={30}
+                />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
