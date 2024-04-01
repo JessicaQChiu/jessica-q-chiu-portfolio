42963252301d578ff6302d9cec0f5c98584814b6
Jess <Jessica.Q.Chiu@gmail.com>
2024-04-01 14:08:13 -0700

    Updated sidebar nav styling
    Updated sidebar nav styling and added a couple of placeholder buttons

--- src/app/components/sidebar/nav/index.tsx
+++ src/app/components/sidebar/nav/index.tsx
@@ -18,65 +18,106 @@ const Nav = ({ toggle }: { toggle: () => void }) => {
             />
           </Link>
         </div>
-        <div className="flex flex-col mb-6 px-4 py-2 bg-white border-2 border-black rounded-lg shadow-[5px_5px_0px_0px_#fed7aa]">
+        <div className="flex flex-col mb-6 px-4 py-2 bg-white border-2 border-black shadow-[5px_5px_0px_0px_#fed7aa]">
           <p>
             Hey there! I&apos;m Jess, a Vancouver-based developer and
             illustrator.
           </p>
         </div>
-        <div className="flex flex-col px-4 py-2 bg-blue-200 border-2 border-black rounded-lg shadow-[5px_5px_0px_0px_#fed7aa]">
-          <ul className="text-black">
-            <li className="flex w-full justify-center items-center bg-white border-2 border-black rounded-lg">
-              <Link href="/about">
-                <p>ABOUT</p>
+        <div className="flex flex-col bg-white border-2 border-black shadow-[5px_5px_0px_0px_#fed7aa]">
+          <div className="flex w-full px-4 py-2 bg-blue-200 border-b-2 border-black">
+            <p>Navigation</p>
+          </div>
+          <ul className="px-4 py-2 text-black">
+            <li className="flex items-center border-2 border-white hover:border-dotted hover:border-black active:text-white active:bg-blue-950">
+              <Link href="/" className="flex row w-full">
+                <Image
+                  src="/images/star.png"
+                  alt="Logo"
+                  width={25}
+                  height={25}
+                />
+                <p>Home.exe</p>
               </Link>
             </li>
-            <li className="flex w-full justify-center items-center bg-white border-2 border-black rounded-lg my-1">
-              <Link href="/portfolio">
-                <p>PORTFOLIO</p>
+            <li className="flex ml-6 w-42 items-center bg-white border-l-2 border-dotted border-black hover:border-hidden active:text-white active:bg-blue-950">
+              <Link
+                href="/about"
+                className="flex row w-full border-2 border-white hover:border-dotted hover:border-black hover:pl-0.5"
+              >
+                <Image
+                  src="/images/starline.png"
+                  alt="Logo"
+                  width={35}
+                  height={25}
+                />
+                <p>About</p>
               </Link>
             </li>
-            <li className="flex w-full justify-center items-center bg-white border-2 border-black rounded-lg">
-              <Link href="/contact">
-                <p>CONTACT</p>
+            <li className="flex ml-6 w-42 items-center bg-white border-l-2 border-dotted border-black hover:border-hidden active:text-white active:bg-blue-950">
+              <Link
+                href="/portfolio"
+                className="flex row w-full border-2 border-white hover:border-dotted hover:border-black hover:pl-0.5"
+              >
+                <Image
+                  src="/images/starline.png"
+                  alt="Logo"
+                  width={35}
+                  height={25}
+                />
+                <p>Portfolio</p>
+              </Link>
+            </li>
+            <li className="flex ml-6 w-42 items-center bg-white border-l-2 border-dotted border-black hover:border-hidden active:text-white active:bg-blue-950">
+              <Link
+                href="/contact"
+                className="flex row w-full border-2 border-white hover:border-dotted hover:border-black hover:pl-0.5"
+              >
+                <Image
+                  src="/images/starline.png"
+                  alt="Logo"
+                  width={35}
+                  height={25}
+                />
+                <p>Contact</p>
               </Link>
             </li>
           </ul>
-          <div className="flex flex-row justify-between pt-2">
+        </div>
+        <div className="flex flex-col mt-6 p-2 bg-white border-2 border-black shadow-[5px_5px_0px_0px_#fed7aa]">
+          <div className="flex flex-row justify-between">
             <div>
-            <Link href="/">
+              <Link href="/" className="flex flex-col px-1 border-2 border-white hover:border-dotted hover:border-black">
                 <Image
                   src="/images/star.png"
                   alt="Logo"
                   width={30}
                   height={30}
+                  style={{margin: "0 auto"}}
                 />
+                <p className="text-sm text-center">Dark<br/>Mode</p>
               </Link>
             </div>
             <div className="flex flex-row">
-              <Link href="/">
+              <Link href="/" className="flex flex-col px-1 border-2 border-white hover:border-dotted hover:border-black">
                 <Image
                   src="/images/star.png"
                   alt="Logo"
                   width={30}
                   height={30}
+                  style={{margin: "0 auto"}}
                 />
+                <p className="text-sm text-center">Reload</p>
               </Link>
-              <Link href="/">
+              <Link href="/" className="flex flex-col px-1 w-12 border-2 border-white hover:border-dotted hover:border-black">
                 <Image
                   src="/images/star.png"
                   alt="Logo"
                   width={30}
                   height={30}
+                  style={{margin: "0 auto"}}
                 />
-              </Link>
-              <Link href="/">
-                <Image
-                  src="/images/star.png"
-                  alt="Logo"
-                  width={30}
-                  height={30}
-                />
+                <p className="text-sm text-center">Top</p>
               </Link>
             </div>
           </div>
