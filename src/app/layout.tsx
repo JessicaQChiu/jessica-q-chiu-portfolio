78847f71d86811442c43c7ba82d20e5d2ccf1a6b
Jess <Jessica.Q.Chiu@gmail.com>
2024-04-02 02:06:02 -0700

    Update layout styling and metadata
    Updated layout styling to fix tablet/mobile view. Added additional metadata for social media previews and SEO.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,23 @@ const spaceMono = Space_Mono({
 export const metadata: Metadata = {
   title: "Jessica Q Chiu - Portfolio",
   description: "Porfolio website of Jessica Q Chiu",
+  metadataBase: new URL('https://jqc-portfolio.vercel.app/'),
+  robots: 'follow, index',
+  openGraph: {
+    title: 'Jessica Q Chiu - Portfolio',
+    description: 'Porfolio website of Jessica Q Chiu',
+    url: 'https://jqc-portfolio.vercel.app/',
+    siteName: 'Jessica Q Chiu Portfolio',
+    images: [
+      {
+        url: '../public/images/computer-icon.png',
+        width: 200,
+        height: 160,
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
@@ -32,10 +49,11 @@ export default function RootLayout({
         <div className="flex justify-center pt-16">
           <SidebarNav />
           <div className="flex flex-col md:ml-6 lg:ml-20 mb-12">
-            <div className="max-w-2xl md:max-w-md lg:max-w-lg bg-white border-2 border-black shadow-[8px_8px_0px_0px_#fed7aa]">
+            <div className="max-w-2xl md:max-w-md lg:max-w-xl m-8 md:m-0 bg-white border-2 border-black shadow-[8px_8px_0px_0px_#fed7aa]">
               <Titlebar />
               <div className="px-2 pb-4">{children}</div>
             </div>
+            <p className="mt-4 text-end text-sm">Built with ♡ in 2024 by Jessica Q Chiu</p>
           </div>
         </div>
       </body>
