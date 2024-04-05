225260d8b97ee3eab772847b0ce25d4a66a1d69a
Jess <Jessica.Q.Chiu@gmail.com>
2024-04-05 04:37:05 -0700

    Updated styling
    Updated to have an even space around the landing image.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <div className="p-2">
       <h1>HOME PAGE</h1>
-      <div className="flex">
+      <div className="flex mt-2">
         <Image src="/images/landing-img.png" alt="Text that says Jessica Q Chiu - developer and illustrator and an image of a home, baba yaga's hut" width={675} height={750} />
       </div>
     </div>
